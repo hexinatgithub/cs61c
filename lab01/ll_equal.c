272b849a897aba8004a6dee9ca6d1656c892c5f7
hexin <hexin.work@outlook.com>
2018-10-24 17:54:29 +0800

    lab01:Exercise 4

--- lab01/ll_equal.c
+++ lab01/ll_equal.c
@@ -7,7 +7,7 @@ typedef struct node {
 
 /* FIXME: this function is buggy. */
 int ll_equal(const node* a, const node* b) {
-	while (a != NULL) {
+	while (a != NULL && b != NULL) {
 		if (a->val != b->val)
 			return 0;
 		a = a->next;
